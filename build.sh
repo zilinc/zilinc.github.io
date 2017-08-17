3f6a23263b40e6b6d39d4a4f3ef0c030ab77fb58
z3350478 <z3350478@cse.unsw.edu.au>
2017-08-17 22:39:30 +1000

    use full path

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-export PATH="/bin:/usr/bin:/usr/local/bin"
-pandoc index.md -f markdown -t html -s -o ~/public_html/index.html
+/usr/bin/pandoc index.md -f markdown -t html -s -o ~/public_html/index.html
