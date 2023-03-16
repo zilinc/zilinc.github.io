489c220ebb6e20c1f0cabecf5b1f1db43c471f10
Zilin Chen <zilinc@gh.st>
2023-03-17 01:55:08 +1100

    update homepage with phd

--- build_pubs.sh
+++ build_pubs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-bibtex2html -o pubs -r -noabstract -nokeywords -s plain references.bib
+bibtex2html -m unicodes.txt -css bib.css -o pubs -r -noabstract -nokeywords -s plain references.bib
